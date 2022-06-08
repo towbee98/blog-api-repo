3249301fb00e47f4994d05863a00d80ce5715325
Oladele Tobiloba <tobiemma200@gmail.com>
2022-06-08 14:21:31 +0100

    refactor the config file to hold prod and development

--- config/env.js
+++ config/env.js
@@ -1,13 +1,11 @@
 const dotenv = require("dotenv");
 dotenv.config();
-//console.log(process.env.NODE_ENV);
+console.log(process.env.NODE_ENV.trim(), 1);
 module.exports = {
   MONGO_URI:
-    // process.env.NODE_ENV === "development"
-    // ?
-    process.env.MONGO_URI_LOCAL,
-  //:
-  //  process.env.MONGO_URI,
+    process.env.NODE_ENV === "development"
+      ? process.env.MONGO_URI_LOCAL
+      : process.env.MONGO_URI,
   PORT: process.env.PORT,
   ENV: process.env.NODE_ENV,
 };
